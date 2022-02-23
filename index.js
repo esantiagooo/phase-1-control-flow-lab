11da3cd8defbe5b14a237e2973f31bbd7465976e
Eric <eric.yanez50@qmail.cuny.edu>
2022-02-23 17:15:58 -0500

    commit tree 240866901972e6d4a7a111859b5fd16653638334

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
-}
+  if(x <= 400){
+    return "This one is on me!"
+  }else if (x > 2500){
+    return "No can do."
+  }else{
+    return "I will gladly take your thirty bucks."
+  }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+}
 }
